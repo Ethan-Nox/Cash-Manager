a105ffa3a8d3e552a1c723da6c8fac6bd25bba62
LucasOgrodnik <lucas.ogrodnik@epitech.eu>
2022-11-21 12:39:20 +0100

    [BACKEND]: Login/Register giving token, no verification for now. (Need to clean the code)

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException, Request, Response
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from core.database import SessionLocal, Base, engine
 from schemas import user_schema
@@ -20,7 +21,6 @@ async def db_session_middleware(request: Request, call_next):
 
 def get_db(request: Request):
     return request.state.db
-
 
 @app.post("/users/", response_model=user_schema.User)
 def create_user(user: user_schema.UserCreate, db: Session = Depends(get_db)):
@@ -49,3 +49,46 @@ def delete_user(user_id: str, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user_view.delete_user(db, user_id=user_id)
+
+
+from fastapi import status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import timedelta
+from views.user_view import login
+from core.jwt import LoginResponse, create_access_token
+import json
+from uuid import UUID
+
+class UUIDEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, UUID):
+            # if the obj is uuid, we simply return the value of uuid
+            return obj.hex
+        return json.JSONEncoder.default(self, obj)
+
+@app.post("/login", response_model=LoginResponse)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = login(db, email = form_data.username, password = form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(
+        data={"sub": json.dumps(user.id, cls=UUIDEncoder)}, expires_delta=access_token_expires
+    )
+    return {"token": {"access_token": access_token, "token_type": "bearer"}, "user": user}
+
+@app.post("/register", response_model=LoginResponse)
+async def register_for_access_token(user: user_schema.UserCreate, db: Session = Depends(get_db)):
+    db_user = user_view.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    user = user_view.create_user(db=db, user=user)
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(
+        data={"sub": json.dumps(user.id, cls=UUIDEncoder)}, expires_delta=access_token_expires
+    )
+    return {"token": {"access_token": access_token, "token_type": "bearer"}, "user": user}
