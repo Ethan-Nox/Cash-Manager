b2356bf7a122d6584f014a2d6bfa296a334e634d
LucasOgrodnik <lucas.ogrodnik@epitech.eu>
2022-11-28 10:48:16 +0100

    [BACKEND] Merging Articles

--- api/main.py
+++ api/main.py
@@ -2,6 +2,8 @@ from fastapi import Depends, FastAPI, Request, Response
 from core.database import SessionLocal, Base, engine
 from routes import user_route, auth_route
 from core import jwt
+from sqladmin import Admin, ModelView
+from models import user_model
 
 Base.metadata.create_all(bind=engine)
 
@@ -21,3 +23,9 @@ async def db_session_middleware(request: Request, call_next):
     finally:
         request.state.db.close()
     return response
+
+admin = Admin(app, engine)
+class UserAdmin(ModelView, model=user_model.User):
+    column_list = [user_model.User.id, user_model.User.email]
+
+admin.add_view(UserAdmin)
